6963da891bc40529668586814f5f9efe208bf5ac
raydon <auk7495@gmail.com>
2025-09-07 21:52:09 +0100

    Update back.js

--- back.js
+++ back.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
   arrow.className = "back-arrow";
   document.body.appendChild(arrow);
 });
+
+// skid preventation
+document.addEventListener("keydown", function (e) {
+    if (
+        e.key === "F12" ||                              // F12
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") || // Ctrl+Shift+I
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "j")    // Ctrl+Shift+J
+    ) {
+        alert("Stop skidding dude!");
+        window.location.href = "about:blank"; // close/redirect page
+    }
+});
